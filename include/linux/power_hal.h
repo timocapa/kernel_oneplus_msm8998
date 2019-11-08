cbd0fc501f66e74da8b7149d917718ac8892da09
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-11-09 05:24:48 +0530

    devfreq: make it possible to dynamically control some CPUBW tunables on-the-go
    Commit reference: https://android.googlesource.com/device/google/coral/+/dbf8bda8a4ca6056de99fc78774f6cf25979af79
    
    Test:
    Boot and check value in path using a terminal app during interactions and app launch.
    
    Paths:
    CPUBWHystTriggerCount: /sys/devices/soc/soc:qcom,cpubw/devfreq/soc:qcom,cpubw/bw_hwmon/hyst_trigger_count
    CPUBWHistMemory: /sys/devices/soc/soc:qcom,cpubw/devfreq/soc:qcom,cpubw/bw_hwmon/hist_memory
    CPUBWHystLength: /sys/devices/soc/soc:qcom,cpubw/devfreq/soc:qcom,cpubw/bw_hwmon/hyst_length
    
    Command:
    while true; do cat <path>; sleep 0.1; done
    
    Behaviour: Toggles correctly.
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>

--- include/linux/power_hal.h
+++ include/linux/power_hal.h
@@ -11,4 +11,19 @@
 /* bool to detect if currently boosted by cpu_input_boost_kick_max() */
 extern bool max_boost_status;
 
+/* CPUBW management */
+#ifdef CONFIG_DEVFREQ_GOV_QCOM_BW_HWMON
+extern unsigned int hyst_trigger_count_val;
+extern unsigned int hist_memory_val;
+extern unsigned int hyst_length_val;
+
+void set_hyst_trigger_count_val(int val);
+void set_hist_memory_val(int val);
+void set_hyst_length_val(int val);
+#else
+static inline void set_hyst_trigger_count_val(int val) { }
+static inline void set_hist_memory_val(int val) { }
+static inline void set_hyst_length_val(int val) { }
+#endif
+
 #endif /* _POWER_HAL_H */
